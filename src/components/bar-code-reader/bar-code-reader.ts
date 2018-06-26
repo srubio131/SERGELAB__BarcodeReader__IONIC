3839ba7c762fc69d57b3bb353551dacd30b50558
Sergio Rubio Anaya <s.rubio131@gmail.com>
2018-06-26 17:00:52 +0200

    Now can print qr code in window

--- src/components/bar-code-reader/bar-code-reader.ts
+++ src/components/bar-code-reader/bar-code-reader.ts
@@ -10,26 +10,46 @@ export class BarCodeReaderComponent {
   encodedData: any = {};
   scannedData: any = {};
 
-  constructor(private barcodeScanner: BarcodeScanner) {
+  constructor(private _barcodeScanner: BarcodeScanner) {
+
+    // Prueba de escaneo
+    this.scan();
+
   }
 
   public scan() {
     let options: BarcodeScannerOptions = {
       prompt: 'Scan you barcode'
     };
-    this.barcodeScanner.scan(options).then(data => {
-      this.scannedData = data;
-      console.log('Datos escaneados: ', data);
+    this._barcodeScanner.scan(options).then(data => {
+
+      this.scannedData = {};
+      if (data && data.text) {
+        this.scannedData = {
+          format: data.format,
+          cancelled: data.cancelled,
+          srcBase64: data.text
+        };
+      }
+
     }).catch(err => {
       console.log('Error al escanear', err);
     });
   }
 
   public encodeText(text) {
-    let type:string = this.barcodeScanner.Encode.TEXT_TYPE;
-    this.barcodeScanner.encode(type, text).then(data => {
-      this.encodedData = data;
-      console.log('Datos encodeados: ', data);
+    let type:string = this._barcodeScanner.Encode.TEXT_TYPE;
+    this._barcodeScanner.encode(type, text).then(data => {
+
+      this.encodedData = {};
+      if (data && data.text) {
+        this.encodedData = {
+          format: data.format,
+          cancelled: data.cancelled,
+          srcBase64: data.text
+        };
+      }
+
     }).catch(err => {
       console.log('Error al encodear', err);
     });
